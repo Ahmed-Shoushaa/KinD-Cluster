24967438190ffef2bac927c7c648372961f96ad9
ahmed <ahmed.adel1199@yahoo.com>
2023-04-10 08:44:55 +0200

    Adding Terraform (KinD EC2 & it's userdata)

---
+++ Terraform/userdata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Download Kubectl command: 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+#install Docker 
+sudo apt-get update && sudo apt-get install docker.io -y
+sudo usermod -aG docker $USER
+sudo chmod 666 /var/run/docker.sock
+
+#install KinD command to create KinD clusters 
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.18.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+#Create KinD Cluster
+kind create cluster --name my-app-cluster
+
+#Install terraform
+sudo snap install terraform --classic
+
+
