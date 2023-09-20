f9c6f882140558f8107164ec1515d2dfafd2937e
Ahmed Adel Shousha <117172376+Ahmed-Shoushaa@users.noreply.github.com>
2023-09-20 11:45:01 +0300

    Update userdata.sh (docker installation part)

--- Terraform/userdata.sh
+++ Terraform/userdata.sh
@@ -6,9 +6,20 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 #install Docker 
-sudo apt-get update && sudo apt-get install docker.io -y
-sudo snap install docker
-sudo usermod -aG docker $USER
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get updatesudo usermod -aG docker $USER
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo chmod 666 /var/run/docker.sock
 
 #install KinD command to create KinD clusters 
